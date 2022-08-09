b38f18a64d189a9c01bb1a9be48036726c3cbe70
Prof Z <professor.zubenelgenobi@gmail.com>
2022-08-08 21:23:59 -0600

    HTML for Nav bar and Content moved from App.js to Navbar.js and Content.js. Selector added to check for both contracts loaded (Token and Exchange) andwired to be used from App.js. See console.log for >>>Contracts loaded?

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -4,5 +4,14 @@ import { createSelector } from 'reselect'
 const account = state => get(state,'web3.account')  // fetch account from store
 export const accountSelector = createSelector(account, a => a) // simple selector that does not operate on the item fetched. It just returns it
 
+const tokenLoaded = state => get(state, 'token.loaded', false)
+export const tokenLoadedSelector = createSelector(tokenLoaded, tl => tl)
 
+const exchangeLoaded = state => get(state, 'exchange.loaded', false)
+export	const exchangeLoadedSelector = createSelector(exchangeLoaded, el => el)
 
+export const contractsLoadedSelector = createSelector(
+	tokenLoaded,
+	exchangeLoaded,
+	(tl, el) => (tl && el)
+)
